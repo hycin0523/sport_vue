bbf8132d635158a8271e64c7813de3c8090821b2
侯俊宇 <260470264@qq.com>
2024-03-08 14:08:03 +0800

    食物分类管理

--- src/views/food/index.vue
+++ src/views/food/index.vue
@@ -1,55 +1,32 @@
 <template>
   <div>
-
+    <el-tabs type="border-card">
+      <el-tab-pane label="分类管理">
+        <type :active="0" />
+      </el-tab-pane>
+      <el-tab-pane label="食物管理">
+        <food :active="1" />
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
+import type from "@/views/food/type.vue";
+import food from "@/views/food/food.vue";
 
 export default {
-  components: {},
+  components: {
+    type,
+    food
+  },
+
   data() {
-    return {};
-  },
-  computed: {},
-  watch: {},
-  methods: {},
-  beforeCreate() {
-  },
-
-  created() {
-
-  },
-
-  beforeMount() {
-
-  },
-
-  mounted() {
-
-  },
-
-  beforeUpdate() {
-
-  },
-
-  updated() {
-
-  },
-
-  beforeDestroy() {
-
-  },
-  destroyed() {
-
-  },
-
-  activated() {
-
+    return {
+      active: 0
+    };
   }
 };
-
-
 </script>
 
 <style scoped>
